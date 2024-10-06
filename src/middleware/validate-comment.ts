ff115e338459ef860c40e3d891c515c944978cc9
FilipeDiass <diasjoaum@gmail.com>
2024-10-06 18:20:52 -0300

    add validate router post comments

---
+++ src/middleware/validate-comment.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod'
+import type { Request, Response, NextFunction } from 'express'
+
+const commentSchema = z
+	.object({
+		name: z.string().min(3, 'O nome deve ter pelo menos 3 caracteres.'),
+		githubUser: z.string().optional(),
+		email: z
+			.string()
+			.email()
+			.min(3, 'O email deve ter pelo menos 3 caracteres.'),
+		comment: z
+			.string()
+			.min(3, 'O comentário deve ter pelo menos 3 caracteres.')
+			.max(700, 'O comentário não pode ter mais que 700 caracteres.'),
+	})
+	.strict()
+
+export function validateComment(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+): void {
+	const data = commentSchema.safeParse(req.body)
+
+	if (data.error) {
+		res.status(400).json({
+			message: 'Erro de validação',
+			details: data.error.errors,
+		})
+		return
+	}
+
+	next()
+}
